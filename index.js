2701cf67ced12050688560919ece4a1cd3961216
Adil Ihsan <adil.ihsan@integriti.io>
2021-05-07 21:21:22 +0500

    make it better

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import PostRoutes from "./routes/posts.js";
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config()
+
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+
+app.use(cors());
+
+// you will get errors or warnings in console if you don't add this object
+app.use("/posts", PostRoutes)
+app.get('/', (req, res) => {
+    res.send("Hello to  my memories app!")
+});
+
+//const ConnectURL = 'mongodb+srv://firstMongoDB:Welcome12@cluster0.1iiwm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT;
+
+//mongoose accepts two params URL and an object
+mongoose.connect(process.env.ConnectURL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=> app.listen(PORT, ()=> console.log(`Server running at port: ${PORT}`)))
+.catch(error=>console.log(error.message))
+
+mongoose.set('useFindAndModify', false);
+
